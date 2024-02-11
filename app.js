7353ddd59f66936e3d136ae9969f40c45bc8ab57
Cheedubem <adimonyechidubem@gmail.com>
2024-02-11 08:22:33 +0100

    Defined Regex patterns for visa and master card

---
+++ app.js
@@ -0,0 +1,5 @@
+function validateCreditCard(cardNumber) {
+  // Define regex patterns for Visa and Mastercard
+  const visaPattern = /^4[0-9]{12}(?:[0-9]{3})?$/;
+  const mastercardPattern = /^5[1-5][0-9]{14}$/;
+}
