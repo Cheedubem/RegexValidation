40997ef987766298902f46707d38fe6be3cdc6c8
Cheedubem <adimonyechidubem@gmail.com>
2024-02-11 08:24:00 +0100

    testing input against the regex patterns

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ function validateCreditCard(cardNumber) {
   // Define regex patterns for Visa and Mastercard
   const visaPattern = /^4[0-9]{12}(?:[0-9]{3})?$/;
   const mastercardPattern = /^5[1-5][0-9]{14}$/;
+
+  // Test the input card number against each pattern
+  if (visaPattern.test(cardNumber)) {
+    return "Visa";
+  } else if (mastercardPattern.test(cardNumber)) {
+    return "Mastercard";
+  } else {
+    return "Invalid";
+  }
 }
